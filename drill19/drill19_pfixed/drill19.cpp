cad9702ab3d0d3709a9ebb6f1f7535969bf08e96
Gellén Rebeka <91344916+redfrog66@users.noreply.github.com>
2022-05-03 21:43:58 +0200

    Add files via upload

--- drill19/drill19_pfixed/drill19.cpp
+++ drill19/drill19_pfixed/drill19.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
+#include<string> 
 #include<vector>
 
 //1. Define template<typename T> struct S { T val; };.
 //2. Add a constructor, so that you can initialize with a T.
 
 using namespace std;
+
 template<typename T>
-struct S{
-	S(T vv = 0) : val{vv} {}
+struct S {
+	S(T vv = 0) : val(vv) {}
 	
 //5. Add a function template get() that returns a reference to val.
 	T& get();
@@ -16,10 +18,10 @@ struct S{
 	const T& get() const;
 	
  //9. Add a set() function template so that you can change val.
-	void set(T new_t) {val = new_t;}
+	void set(T new_t) {val = new_t; };
 	
 //10. Replace set() with an S<T>::operator=(const T&). Hint: Much simpler than §19.2.5.
-	S<T>& S<T>::operator=(const T& s){}
+	void operator=(const T& s);
 	
         //7. Make val private.
 	private:
@@ -27,19 +29,30 @@ struct S{
 };
 
 template<typename T>
-T& S<T>::get(){return val;}
+T& S<T>::get()
+{
+	return val;
+}
 
 //6. Put the definition of get() outside the class.
 template<typename T>
-const T& S<T>::get() const;
+const T& S<T>::get() const
+{ 
+	return val;
+}
 
 template<typename T>
-S<T>& S<T>::operator=(const T& s){
+void S<T>::operator=(const T& s)
+{
 val = s;
-return *this;}
+}
 
 //12. Define a function template<typename T> read_val(T& v) that reads from cin into v.
-template<typename T> void read_val(T& v){cin >> v;}
+template<typename T> T& read_val(T& s)
+{
+cin >> s;
+return s;
+}
 
 /*14. Bonus: Define input and output operators (>> and <<) for vector<T>s. For both input and output use a { val, val, val }
 format. That will allow read_val() to also handle the S<vector<int>> variable. */
@@ -77,24 +90,15 @@ int main(){
     
     
 //3. Define variables of types S<int>, S<char>, S<double>, S<string>, and S<vector<int>>; initialize them with values of your choice.    
-S<int> s;
 S<int> si {37};
 S<char> sc {'c'};
 S<double> sd {10.2};
 S<string> ss {"alma"};
-S<std::vector<int>> svi {std : vector<int>{1,2,3,4,5}};
+S<std::vector<int>> svi {std :: vector<int>{1,2,3,4,5}};
  
  
 //4. Read those values and print them.
-cout << "S<int> :" << s << endl;
-cout << "S<int> :" << si << endl;
-cout << "S<char> :" << sc << endl;
-cout << "S<double> :" << sd << endl;
-cout << "S<string> :" << ss << endl;
-
-
 //8. Do 4 again using get().
-cout << "S<int> :" << s.get() << endl;
 cout << "S<int> :" << si.get() << endl;
 cout << "S<char> :" << sc.get() << endl;
 cout << "S<double> :" << sd.get() << endl;
@@ -102,28 +106,25 @@ cout << "S<string> :" << ss.get() << endl;
 
 for(auto& a : svi.get()){cout << a << ' ';}
 cout<<endl;
-	
-
-
-sc.set('s');
 
 //13. Use read_val() to read into each of the variables from 3 except the S<vector<int>> variable.
 
-int ii;
-read_val(ii);
-S<int> si2 {ii};
+int i;
+si.set(read_val(i));
 
-char cc;
-read_val(cc);
-S<char> sc2{cc};
+char c;
+sc.set(read_val(c));
 
-double dd;
-read_val(dd);
-S<double> sd2{dd};
+double d;
+sd.set(read_val(d));
 
-cout<< "S<int> :" << si2.get() << endl
-<< "S<char> : " << sc2.get() << endl;
-<< "S<double> :" << sd2.get() << endl;
+string s;
+ss.set(read_val(s));
+
+cout << "S<int> :" << si.get() << endl;
+cout << "S<char> :" << sc.get() << endl;
+cout << "S<double> :" << sd.get() << endl;
+cout << "S<string> :" << ss.get() << endl;
 
 }
 catch(exception& e)
